421bc1835513295e010fc9f8a758c3242e88cbf6
mukondono <nathanielreline@gmail.com>
2015-10-29 17:21:03 -0700

    Changed project folder name

---
+++ Intermediary/app/src/main/java/natrelin/Intermediary/MainActivity.java
@@ -0,0 +1,114 @@
+package natrelin.Intermediary;
+
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity  implements TextToSpeech.OnInitListener {
+
+    private EditText input;
+    private TextToSpeech tts;
+    private Button speak;
+    private Locale userLocale;
+    private Locale[] availableLanguages;
+
+    private final String TAG = "MAINACTIVITY";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        availableLanguages = Locale.getAvailableLocales(); // get all of the available locales
+        userLocale = Locale.JAPAN; // create a default voice
+
+        tts = new TextToSpeech(this, this);
+
+        input = (EditText) findViewById(R.id.edittext);
+        // automatically bring up the keyboard when the edittext is focused
+        input.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+                }
+            }
+        });
+
+        speak = (Button) findViewById(R.id.button);
+        speak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tts.speak(input.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        for(Locale locale : availableLanguages) {
+            // TODO check if the language is installed on the device, if so...
+            //Setting speech language
+            //If your device supports language you set above
+            /** onInit is called after this line... **/
+            //if (tts.setLanguage(locale) == TextToSpeech.LANG_COUNTRY_VAR_AVAILABLE)
+                menu.add(locale.toString());
+        }
+
+        /*menu.add(userLocale.toString());
+        menu.add((Locale.US).toString());*/
+
+        tts.setLanguage(userLocale);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        // set language on click
+        userLocale = new Locale(item.toString());
+        tts.setLanguage(userLocale); // no error if the language is not available, installed languages work fine
+        return super.onOptionsItemSelected(item);
+    }
+
+    // initialize our tts object
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            //Setting speech language
+            int result = tts.setLanguage(userLocale);
+            //If your device doesn't support language you set above
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                //Cook simple toast message with message
+                Toast.makeText(this, "Language not supported", Toast.LENGTH_LONG).show();
+                Log.e("TTS", "Language is not supported");
+            }
+            //Enable the button
+            else {
+                speak.setEnabled(true);
+            }
+            //TTS is not initialized properly
+        } else {
+            Toast.makeText(this, "TTS Initilization Failed", Toast.LENGTH_LONG).show();
+            Log.e("TTS", "Initilization Failed");
+        }
+    }
+}
