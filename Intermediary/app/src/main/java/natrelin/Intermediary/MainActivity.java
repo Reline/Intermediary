7e2b70cedcfe53d4346e23cd873d6873b28cafaf
mukondono <nathanielreline@gmail.com>
2015-11-12 16:01:16 -0700

    Fixed leaked ServiceConnection error and TTS engine connection not fully set up warning

--- Intermediary/app/src/main/java/natrelin/Intermediary/MainActivity.java
+++ Intermediary/app/src/main/java/natrelin/Intermediary/MainActivity.java
@@ -1,6 +1,11 @@
 package natrelin.Intermediary;
 
 import android.annotation.TargetApi;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Build;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
@@ -19,11 +24,16 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity  implements TextToSpeech.OnInitListener {
 
+    // text to speech
     private EditText input;
     private TextToSpeech tts;
     private Button speak;
     private Locale userLocale;
     private Locale[] availableLanguages;
+
+    // accelerometer
+    private SensorManager sensorManager;
+    private SensorEventListener sensorEventListener;
 
     private final String TAG = "MAINACTIVITY";
 
@@ -31,6 +41,8 @@ public class MainActivity extends AppCompatActivity  implements TextToSpeech.OnI
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initAccelerometer(); // set up accelerometer
 
         availableLanguages = Locale.getAvailableLocales(); // get all of the available locales
         userLocale = Locale.JAPAN; // create a default voice
@@ -71,6 +83,41 @@ public class MainActivity extends AppCompatActivity  implements TextToSpeech.OnI
         tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
     }
 
+    private void initAccelerometer() {
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        /*mAccel = 0.00f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;*/
+    }
+
+    /** the accelerometer should be deactivated onPause and activated onResume to save resources **/
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        sensorManager.unregisterListener(sensorEventListener);
+        super.onPause();
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -87,8 +134,6 @@ public class MainActivity extends AppCompatActivity  implements TextToSpeech.OnI
 
         /*menu.add(userLocale.toString());
         menu.add((Locale.US).toString());*/
-
-        tts.setLanguage(userLocale);
 
         return true;
     }
@@ -127,5 +172,17 @@ public class MainActivity extends AppCompatActivity  implements TextToSpeech.OnI
             Toast.makeText(this, "TTS Initilization Failed", Toast.LENGTH_LONG).show();
             Log.e("TTS", "Initilization Failed");
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        //Close the Text to Speech Library
+        if(tts != null) {
+
+            tts.stop();
+            tts.shutdown();
+            Log.d(TAG, "TTS Destroyed");
+        }
+        super.onDestroy();
     }
 }
